089144f78282b8d1947382e7c5be15862a1aff24
Shay_Batonya <127010513+ShaiBatonya@users.noreply.github.com>
2024-08-12 13:47:55 +0300

    v7

--- front/src/config/axiosInstance.tsx
+++ front/src/config/axiosInstance.tsx
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
-
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'https://starquest-upload-production-a0cc.up.railway.app/api',
+  baseURL: process.env.NODE_ENV === 'production'
+    ? 'https://starquest-upload-production-a0cc.up.railway.app/api'
+    : 'http://localhost:6500/api',
   headers: {
     'Content-Type': 'application/json',
   },
